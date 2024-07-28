e122b642d5ab2ebbf115ef912d0265a420861b19
Artem <andrei28123@gmail.com>
2024-07-28 21:54:11 +0200

    Create structure first page and start work with Header

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import AppHeader from '../appHeader/appHeader'
+import './App.scss';
+
+function App() {
+   return (
+      <div className="App">
+         <AppHeader />
+      </div>
+   );
+}
+
+export default App;
---
+++ src/components/appHeader/appHeader.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './appHeader.scss'
+
+const AppHeader = () => {
+   return (
+      <header className='appHeader'>
+         <h1 className="leftHeader">
+            <a href='#'>Marvel</a>
+            <span> information portal</span>
+         </h1>
+         <nav className="rightHeader">
+            <ul>
+               <li><a href='#' className='red'>Characters</a></li>
+               /
+               <a href='#'><li>Comics</li></a>
+            </ul>
+         </nav>
+      </header>
+   )
+}
+
+export default AppHeader;
